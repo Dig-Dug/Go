a26b3af855a33056be4c9b4d44a3923920482961
unknown <40063087+Dig-Dug@users.noreply.github.com>
2023-08-11 20:20:23 +0200

    package?

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "Yrs"
+	var name2 string
+	var name3 string
+
+	var num int = 444
+	name3 += "Go!"
+	fmt.Println("Hello, World!")
+	name = "lplplp"
+	name2 = "name2"
+	fmt.Println(name, name2, name3, num)
+	//fmt.Println(quote.Go())
+}
